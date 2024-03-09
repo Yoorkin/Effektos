2f97d437cd03ae851936887fd1d578a1329625fe
yorkin <cyb_yorkin@outlook.com>
2024-03-10 02:26:16 +0800

    CPSToRTL: process switch expression

--- src/CPSToRTL.hs
+++ src/CPSToRTL.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module CPSToRTL where
 
 import qualified CPS
 import CompileEnv
-import Control.Monad (zipWithM)
-import Control.Monad.State.Lazy
+import qualified Constant
+import Control.Monad (mapAndUnzipM, zipWithM)
+import Control.Monad.Extra (concatMapM)
+import qualified Control.Monad.State.Lazy as State
 import Data.Map.Lazy hiding (map)
 import GHC.Num (integerFromInt)
+import qualified Primitive as Op
 import RTL
 import Prelude hiding (lookup)
 
@@ -15,35 +19,51 @@ type Operands = Map Name Operand
 
 type ValCount = Int
 
-subst, append :: Name -> State (Operands, ValCount) Operand
+type LabelCount = Int
+
+type State a = State.State (Operands, LabelCount, ValCount) a
+
+subst, append :: Name -> State Operand
 subst n = do
-  mp <- fst <$> get
+  (mp, _, _) <- State.get
   case lookup n mp of
     Nothing -> append n
     Just x -> pure x
-append n = state $ \(mp, count) ->
+append n = State.state $ \(mp, label, count) ->
   let operand = Val count
-   in (operand, (insert n operand mp, count + 1))
+   in (operand, (insert n operand mp, label, count + 1))
+
+freshLabel :: State Label
+freshLabel = State.state $ \(a, label, c) -> ("block" ++ show label, (a, label + 1, c))
+
+type BasicBlocks = [BasicBlock]
 
 type FnKont = Name
 
-translValue :: Name -> CPS.Value -> State (Operands, ValCount) [Inst]
+translValue :: Name -> CPS.Value -> State [Inst]
 translValue n1 (CPS.Var n2) = do
-  mov <- Move <$> subst n1 <*> subst n2
-  pure [mov]
+  n1' <- subst n1
+  n2' <- subst n2
+  return [Move n1' n2']
 translValue n (CPS.I32 i) = do
-  mov <- Move <$> subst n <*> pure (I64 $ integerFromInt i)
-  pure [mov]
+  n' <- subst n
+  return [Move n' value]
+  where
+    value = I64 $ integerFromInt i
 translValue n CPS.Unit = do
-  inst <- Move <$> subst n <*> pure Unit
-  pure [inst]
-translValue n (CPS.Tuple xs) = (++) <$> mallocInsts <*> movInsts
+  n' <- subst n
+  return [Move n' Unit]
+translValue n (CPS.Tuple xs) =
+  (++) <$> mallocInsts <*> movInsts
   where
     mallocInsts = do
       v <- subst n
+      label <- freshLabel
       pure
         [ Move (Reg 1) (I64 $ integerFromInt (length xs)),
+          Move RLK (Label label),
           Call "malloc",
+          NewBlock label,
           Move v (Reg 1)
         ]
     movInsts =
@@ -55,12 +75,18 @@ translValue n (CPS.Tuple xs) = (++) <$> mallocInsts <*> movInsts
         xs
 translValue _ _ = undefined
 
-translExpr :: Maybe FnKont -> CPS.Term -> State (Operands, ValCount) [Inst]
-translExpr fnk (CPS.LetVal n l t) = (++) <$> translValue n l <*> translExpr fnk t
-translExpr fnk (CPS.LetSel n1 i n2 t) = (:) <$> inst <*> insts
+translExpr :: Maybe FnKont -> CPS.Term -> State [Inst]
+translExpr fnk (CPS.LetVal n l t) = do
+  valueInsts <- translValue n l
+  remainInsts <- translExpr fnk t
+  return (valueInsts ++ remainInsts)
+translExpr fnk (CPS.LetSel n1 i n2 t) = do
+  n1' <- subst n1
+  n2' <- subst n2
+  insts <- translExpr fnk t
+  return (Load n1' n2' index : insts)
   where
-    inst = Load <$> subst n1 <*> subst n2 <*> pure (I64 $ integerFromInt $ i * 8)
-    insts = translExpr fnk t
+    index = I64 (integerFromInt (i * 8))
 translExpr _ (CPS.LetCont {}) = error "invalid input"
 translExpr _ (CPS.LetConts {}) = error "invalid input"
 translExpr _ (CPS.LetFns {}) = error "invalid input"
@@ -68,12 +94,19 @@ translExpr fnk (CPS.Continue k x)
   | Just k' <- fnk,
     k' == k = do
       x' <- subst x
-      pure [Move (Reg 1) x', Ret]
+      return
+        [ Move (Reg 1) x',
+          Ret
+        ]
   | otherwise = do
       x' <- subst x
-      pure [Move (Reg 1) x', Goto (Label (show k))]
+      return
+        [ Move (Reg 1) x',
+          Goto (Label (show k))
+        ]
 translExpr _ (CPS.Apply _ _ Nothing _) = undefined
-translExpr _ (CPS.Apply f k (Just env) xs) = (++) <$> argsMovInst <*> callInst
+translExpr _ (CPS.Apply f k (Just env) xs) = do
+  (++) <$> argsMovInst <*> callInst
   where
     argsMovInst =
       zipWithM
@@ -89,33 +122,67 @@ translExpr _ (CPS.Apply f k (Just env) xs) = (++) <$> argsMovInst <*> callInst
         [ Move RLK k',
           Goto f'
         ]
-translExpr _ (CPS.LetPrim n op xs t) = undefined
-translExpr _ (CPS.Switch n cs ts fb) = undefined
+translExpr fnk (CPS.LetPrim n op args t) = do
+  inst <- pmt op args
+  insts <- translExpr fnk t
+  return (inst : insts)
+  where
+    binary o a b = Binary <$> subst n <*> pure o <*> subst a <*> subst b
+    unary o a = Unary <$> subst n <*> pure o <*> subst a
+    pmt Op.Add2 [a, b] = binary Add2 a b
+    pmt Op.Sub2 [a, b] = binary Sub2 a b
+    pmt Op.EQ [a, b] = binary RTL.EQ a b
+    pmt Op.Sub1 [a] = unary Sub1 a
+    pmt a b = error $ show (a, b)
+translExpr fnk (CPS.Switch n cs ts fb) = do
+  (testInstr, caseInstr) <- mapAndUnzipM translCase (zip cs ts)
+  fallbackInstr <- getFallbackInstr
+  return (testInstr ++ fallbackInstr ++ concat caseInstr)
+  where
+    translConst = \case
+      (Constant.Integer i) -> (I64 $ integerFromInt i)
+      Constant.Unit -> Unit
+    translCase (const, term) = do
+      n' <- subst n
+      caseLabel <- freshLabel
+      caseInstr <- translExpr fnk term
+      let testInstr = Jeq n' (translConst const) (Label caseLabel)
+      let caseBlock = NewBlock caseLabel : caseInstr
+      return (testInstr, caseBlock)
+    getFallbackInstr = do
+      case fb of
+        Nothing -> return [] -- should be error
+        Just fb' -> translExpr fnk fb'
 translExpr _ (CPS.Halt x) = do
   x' <- subst x
-  pure [Move (Reg 1) x', Call "exit"]
+  return
+    [ Move (Reg 1) x',
+      Call "exit"
+    ]
 
-translCont :: Maybe FnKont -> (Name, CPS.Value) -> State (Operands, ValCount) BasicBlock
+translCont :: Maybe FnKont -> (Name, CPS.Value) -> State [Inst]
 translCont fnk (n, CPS.Cont x t) = do
-  v <- append x
-  let inst = Move v (Reg 1)
+  inst <- Move <$> append x <*> pure (Reg 1)
   insts <- translExpr fnk t
-  return $ BasicBlock (show n) (inst : insts)
+  return
+    ( NewBlock (show n)
+        : inst
+        : insts
+    )
 translCont _ _ = error "invalid input"
 
 labelMap :: (Ord a, Show a) => [a] -> Map a Operand
 labelMap names = fromList (map (\n -> (n, Label $ show n)) names)
 
 translFn :: [Name] -> (Name, CPS.Value) -> Fn
-translFn contFnNames (n, CPS.Fn k (Just env) xs t) = Fn (show n) allBlocks
+translFn contFnNames (n, CPS.Fn k (Just env) xs t) = Fn (show n) [entryBlock]
   where
-    initState = (labelMap contFnNames, 0)
-    allBlocks = flip evalState initState $ do
-      blocks <- mapM (translCont (Just k)) conts
-      inst <- zipWithM (\i x -> Move <$> append x <*> pure (Reg i)) [1 ..] (env : xs)
-      insts <- translExpr (Just k) t'
-      let entry = BasicBlock "start" (inst ++ insts)
-      pure (entry : blocks)
+    initState = (labelMap contFnNames, 0, 0)
+    entryBlock = flip State.evalState initState $ do
+      contInst <- concatMapM (translCont (Just k)) conts
+      argSavingInst <- zipWithM (\i x -> Move <$> append x <*> pure (Reg i)) [1 ..] (env : xs)
+      entryInst <- translExpr (Just k) t'
+      return $ BasicBlock "start" (NewBlock "start" : argSavingInst ++ entryInst ++ contInst)
     (conts, t') =
       case t of
         (CPS.LetConts cs m) -> (cs, m)
@@ -125,10 +192,11 @@ translFn _ _ = error "invalid input"
 translate :: CPS.Term -> Program
 translate t = Program fns' mainBlocks
   where
-    mainBlocks = flip evalState (labelMap . collectFnContNames $ t, 0) $ do
-      blocks <- mapM (translCont Nothing) conts
-      expr <- translExpr Nothing t2
-      pure (BasicBlock "start" expr : blocks)
+    mainBlocks = flip State.evalState (labelMap . collectFnContNames $ t, 0, 0) $ do
+      contInstr <- concatMapM (translCont Nothing) conts
+      entryInstr <- translExpr Nothing t2
+      let instr = NewBlock "start" : entryInstr ++ contInstr
+      return [BasicBlock "start" instr]
     fns' = map (translFn labels) fns
     labels = collectFnContNames t
     (fns, t1) =
--- src/RTL.hs
+++ src/RTL.hs
@@ -6,7 +6,7 @@ import Prettyprinter.Render.String
 
 data Program = Program [Fn] [BasicBlock]
 
-data Fn = Fn Label [BasicBlock] deriving Show
+data Fn = Fn String [BasicBlock] deriving Show
 
 type Label = String
 
@@ -37,6 +37,7 @@ data Inst
   | Goto Operand
   | Jeq Operand Operand Operand 
   | Call String 
+  | NewBlock Label
   | Ret 
   deriving (Show)
 
@@ -44,10 +45,10 @@ data Inst
 
 instance Pretty Operand where
   pretty (Reg i) = pretty $ "r" ++ show i
-  pretty RLK = pretty "RTL"
+  pretty RLK = pretty "rtl"
   pretty (Val i) = pretty $ "v" ++ show i
-  pretty (Label n) = parens (pretty "label" <+> pretty n)
-  pretty (I64 i) = pretty (show i ++ "L")
+  pretty (Label n) = pretty n
+  pretty (I64 i) = pretty (show i)
   pretty Unit = pretty "unit"
  
 instance Pretty BinOp where
@@ -57,18 +58,19 @@ instance Pretty UryOp where
   pretty = pretty . show
 
 instance Pretty Inst where
-  pretty (Binary a op b c) = pretty a <=> pretty b <+> pretty op <+> pretty b
-  pretty (Unary a op b) = pretty a <=> pretty op <+> pretty b
-  pretty (Move a b) = pretty a <=> pretty b
-  pretty (Load a b c) = pretty a <=> pretty b <> brackets (pretty c)
-  pretty (Store a b c) = pretty a <> brackets (pretty c) <=> pretty b
-  pretty (Goto a) = pretty "goto" <+> pretty a
-  pretty (Jeq a b c) = pretty "jeq" <+> pretty a <+> pretty b <+> pretty c
-  pretty (Call f) = pretty "call" <+> pretty f
-  pretty Ret = pretty "ret"
+  pretty (Binary a op b c) = space <+> (pretty a <=> pretty b <+> pretty op <+> pretty b)
+  pretty (Unary a op b) = space <+> (pretty a <=> pretty op <+> pretty b)
+  pretty (Move a b) = space <+> (pretty a <=> pretty b)
+  pretty (Load a b c) = space <+> (pretty a <=> pretty b <> brackets (pretty c))
+  pretty (Store a b c) = space <+> (pretty a <> brackets (pretty c) <=> pretty b)
+  pretty (Goto a) = space <+> (pretty "goto" <+> pretty a)
+  pretty (Jeq a b c) = space <+> (pretty "jeq" <+> pretty a <+> pretty b <+> pretty c)
+  pretty (Call f) = space <+> (pretty "call" <+> pretty f)
+  pretty (NewBlock n) = pretty n <> pretty ":"
+  pretty Ret = space <+> (pretty "ret")
 
 instance Pretty BasicBlock where
-  pretty (BasicBlock l insts) = pretty l <> pretty ":" <> nest 2 (line <> vsep (map pretty insts)) 
+  pretty (BasicBlock l insts) = pretty l <> pretty ":" <> line <> vsep (map pretty insts) 
 
 
 instance Pretty Fn where
